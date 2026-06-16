62a7893e4a104a97e3009ba2db391e525d6f9686
Solarcemir <lhoifan@uoguelph.ca>
2026-06-16 15:45:26 -0400

    budget functions and zustand globals

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -7,10 +7,17 @@ type AppStore = {
   budgets: Budget[],
   expenses: Expense[],
   addAccount: (account: Account) => void;
-  addBudget: (budget: Budget) => void;
-  addExpense: (expense: Expense) => void;
   deleteAccount: (accountID: string) => void;
   modifyAccount: (updatedAccount: Account) => void;
+
+  addBudget: (budget: Budget) => void;
+  deleteBudget: (budgetID: string) => void;
+  modifyBudget: (updatedBudget: Budget) => void;
+
+  addExpense: (expense: Expense) => void;
+  deleteExpense: (expenseID: string) => void;
+  modifyExpense: (updatedExpense: Expense) => void;
+
 };
 
 export const useAppStore = create<AppStore>((set,get) => ({
@@ -24,29 +31,59 @@ export const useAppStore = create<AppStore>((set,get) => ({
       accounts : [...get().accounts, account]
     }),
 
-  //add an budget
-  addBudget: (budget) => 
-    set({
-      budgets: [...get().budgets, budget]
-    }),
-
-//add an expense
-  addExpense: (expense) =>
-     set({
-      expenses: [...get().expenses,expense]
-    }),
-
     //delete account
    deleteAccount: (accountID) =>
         set({
             accounts: get().accounts.filter(
                 account => account.id !== accountID
-            ),
+            )
         }),
 
     // modifying the account (create a new object first)
     modifyAccount: (updatedAccount) => 
       set({
         accounts: get().accounts.map(account => account.id === updatedAccount.id ? updatedAccount : account)
-      })
+      }),
+
+
+
+        //add an budget
+   addBudget: (budget) => 
+    set({
+      budgets: [...get().budgets, budget]
+    }),
+
+      //delete an budget
+    deleteBudget : (budgetID) => set({
+      budgets: get().budgets.filter(
+        budget => budget.id !== budgetID 
+      )
+    }),
+
+    //modify an budget
+    modifyBudget: (updatedBudget) => set({
+      budgets: get().budgets.map(budget => budget.id === updatedBudget.id ? updatedBudget : budget)
+    }),
+
+    //add an expense
+  addExpense: (expense) =>
+     set({
+      expenses: [...get().expenses,expense]
+    }),
+
+
+  // delete expense
+  deleteExpense: (expenseID) => set({
+    expenses: get().expenses.filter(
+      expense => expense.id !== expenseID
+    )
+  }),
+
+  // modify an expense
+  modifyExpense: (updatedExpense) => set({
+    expenses: get().expenses.map(
+      expense => expense.id === updatedExpense.id ? updatedExpense : expense
+    )
+  })
+
 }));
